3240865dc11e214ddca15c48bcd1902355744f86
UzomaAkpala <francisakpala@yahoo.com>
2023-10-30 10:27:32 +0100

    Create  Badge for number and products in cart

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -2,6 +2,12 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Badge from "react-bootstrap/Badge";
+import Form from "react-bootstrap/Form";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
 
 function HeaderComponent() {
   return (
@@ -11,8 +17,28 @@ function HeaderComponent() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
+            <InputGroup>
+              <DropdownButton
+                id="dropdown-basic-button"
+                title="Dropdown button"
+              >
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
+              <Form.Control type="text" placeholder="Normal text" />
+              <Button variant="warning">Warning</Button>
+            </InputGroup>
+          </Nav>
+          <Nav>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#pricing">
+              <Badge pill bg="danger">
+                2
+              </Badge>
+              CART
+            </Nav.Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -24,12 +50,6 @@ function HeaderComponent() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
