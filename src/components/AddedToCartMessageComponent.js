f2c8619626c22ee9385453611b1499b30e426811
UzomaAkpala <francisakpala@yahoo.com>
2023-10-31 14:12:09 +0100

    Improve Message Component

--- src/components/AddedToCartMessageComponent.js
+++ src/components/AddedToCartMessageComponent.js
@@ -1,15 +1,22 @@
-import { Alert } from "react-bootstrap";
+import { Alert, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const AddedToCartMessageComponent = () => {
   const [show, setShow] = useState(true);
   return (
-    <Alert show={show} variant="danger" onClose={() => setShow(false)} dismissible>
-      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+    <Alert
+      show={show}
+      variant="success"
+      onClose={() => setShow(false)}
+      dismissible
+    >
+      <Alert.Heading>This was added to your cart!</Alert.Heading>
       <p>
-        Change this and that and try again. Duis mollis, est non commodo luctus,
-        nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis
-        consectetur purus sit amet fermentum.
+        <Button variant="success">Go Back</Button>{" "}
+        <Link to="/cart">
+          <Button variant="danger">Go Back</Button>
+        </Link>
       </p>
     </Alert>
   );
