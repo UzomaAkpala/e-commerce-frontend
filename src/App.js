a083a06a9b54cd5f0bb4cdd629ab7ff6ca26c8f5
UzomaAkpala <francisakpala@yahoo.com>
2023-10-31 13:01:23 +0100

    Create Scroll to top functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-//components
+// components:
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 
-// user components
+//user components:
 import RoutesWithUserChatComponent from "./components/user/RoutesWithUserChatComponent";
 
-// publicly available messages
+// publicly available pages:
 import HomePage from "./pages/HomePage";
-import CartPage from "./pages/CartPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 import ProductListPage from "./pages/ProductListPage";
+import CartPage from "./pages/CartPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import ProtectedRoutesComponents from "./components/ProtectedRoutesComponents";
+
+import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent";
 
 // protected user pages:
 import UserProfilePage from "./pages/user/UserProfilePage";
@@ -22,35 +23,39 @@ import UserOrdersPage from "./pages/user/UserOrdersPage";
 import UserCartDetailsPage from "./pages/user/UserCartDetailsPage";
 import UserOrderDetailsPage from "./pages/user/UserOrderDetailsPage";
 
-//protected admin pages:
-import AdminAnalyticsPage from "./pages/admin/AdminAnalyticsPage";
-import AdminChartsPage from "./pages/admin/AdminChartsPage";
-import AdminCreateNewProductPage from "./pages/admin/AdminCreateNewProductPage";
-import AdminEditProductPage from "./pages/admin/AdminEditProductPage";
-import AdminEditUserPage from "./pages/admin/AdminEditUserPage";
-import AdminOrderDetailsPage from "./pages/admin/AdminOrderDetailsPage";
-import AdminOrdersPage from "./pages/admin/AdminOrdersPage";
-import AdminProductsPage from "./pages/admin/AdminProductsPage";
+// protected admin pages:
 import AdminUsersPage from "./pages/admin/AdminUsersPage";
+import AdminEditUserPage from "./pages/admin/AdminEditUserPage";
+import AdminProductsPage from "./pages/admin/AdminProductsPage";
+import AdminCreateProductPage from "./pages/admin/AdminCreateProductPage";
+import AdminEditProductPage from "./pages/admin/AdminEditProductPage";
+import AdminOrdersPage from "./pages/admin/AdminOrdersPage";
+import AdminOrderDetailsPage from "./pages/admin/AdminOrderDetailsPage";
+import AdminChatsPage from "./pages/admin/AdminChatsPage";
+import AdminAnalyticsPage from "./pages/admin/AdminAnalyticsPage";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <HeaderComponent />
       <Routes>
         <Route element={<RoutesWithUserChatComponent />}>
-          {/* publicly available routes */}
+          {/* publicly available routes: */}
           <Route path="/" element={<HomePage />} />
-          <Route path="/cart" element={<CartPage />} />
           <Route path="/product-list" element={<ProductListPage />} />
           <Route path="/product-details" element={<ProductDetailsPage />} />
           <Route path="/product-details/:id" element={<ProductDetailsPage />} />
+          <Route path="/cart" element={<CartPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="*" element="Page not found" />
+          <Route path="*" element="Page not exists 404" />
         </Route>
+        {/* <Route path="/" component={HomePage} />  in previous versions of react-router-dom */}
 
-        <Route element={<ProtectedRoutesComponents admin={false} />}>
+        {/* user protected routes: */}
+        <Route element={<ProtectedRoutesComponent admin={false} />}>
           <Route path="/user" element={<UserProfilePage />} />
           <Route path="/user/my-orders" element={<UserOrdersPage />} />
           <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
@@ -61,30 +66,25 @@ function App() {
         </Route>
 
         {/* admin protected routes: */}
-
-        <Route element={<ProtectedRoutesComponents admin={true} />}>
+        <Route element={<ProtectedRoutesComponent admin={true} />}>
           <Route path="/admin/users" element={<AdminUsersPage />} />
-          <Route path="/admin/analytics" element={<AdminAnalyticsPage />} />
-          <Route path="/admin/charts" element={<AdminChartsPage />} />
+          <Route path="/admin/edit-user" element={<AdminEditUserPage />} />
+          <Route path="/admin/products" element={<AdminProductsPage />} />
           <Route
             path="/admin/create-new-product"
-            element={<AdminCreateNewProductPage />}
+            element={<AdminCreateProductPage />}
           />
           <Route
-            path="/admin/edit-products"
+            path="/admin/edit-product"
             element={<AdminEditProductPage />}
           />
-          <Route path="/admin/edit-user" element={<AdminEditUserPage />} />
+          <Route path="/admin/orders" element={<AdminOrdersPage />} />
           <Route
             path="/admin/order-details"
             element={<AdminOrderDetailsPage />}
           />
-
-          <Route path="/admin/orders" element={<AdminOrdersPage />} />
-
-          <Route path="/admin/products" element={<AdminProductsPage />} />
-
-          <Route path="/admin/users-page" element={<AdminUsersPage />} />
+          <Route path="/admin/chats" element={<AdminChatsPage />} />
+          <Route path="/admin/analytics" element={<AdminAnalyticsPage />} />
         </Route>
       </Routes>
       <FooterComponent />
@@ -93,3 +93,4 @@ function App() {
 }
 
 export default App;
+
