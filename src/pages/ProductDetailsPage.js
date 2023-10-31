27940ebfaf8f8ed1c84289202d70d8bda78d6e9e
UzomaAkpala <francisakpala@yahoo.com>
2023-10-31 13:28:30 +0100

    Prepare Product Details Page

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -1,9 +1,27 @@
-import { useParams } from "react-router-dom";
+import { Row, Col, Container } from "react-bootstrap";
 
-function ProductDetailsPage() {
-  const { id } = useParams();
-  console.log(id);
-  return <h1>Product details page</h1>;
-}
+const ProductDetailsPage = () => {
+  return (
+    <Container>
+      <Row className="mt-5">
+        <Col md={4}>Images</Col>
+        <Col md={8}>
+          <Row>
+            <Col md={8}>product name, price, description, rating</Col>
+            <Col md={4}>product status, quantity</Col>
+          </Row>
+          <Row>
+            <Col className="mt-5">
+              <h5>REVIEWS</h5>
+            </Col>
+          </Row>
+          <hr />
+          send review form
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default ProductDetailsPage;
+
