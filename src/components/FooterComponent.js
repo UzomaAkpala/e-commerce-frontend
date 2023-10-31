af6c5415bd4845e71f1a5c9758f6ff432f3d2c81
UzomaAkpala <francisakpala@yahoo.com>
2023-10-31 11:48:19 +0100

    Improving attributes section for filtering options

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -4,7 +4,7 @@ function FooterComponent() {
   return (
     <footer>
       <Container fluid>
-        <Row className="mt-5">
+        <Row className="mt-5 ">
           <Col className="bg-dark text-white text-center py-5 ">
             Copyright &copy; Best online shop;
           </Col>
