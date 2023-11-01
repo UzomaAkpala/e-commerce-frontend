7e09edebe80f7e87faf955370250c90f7d1a73ed
UzomaAkpala <francisakpala@yahoo.com>
2023-11-01 14:56:07 +0100

    Page for User Order Details

--- src/pages/user/UserOrderDetailsPage.js
+++ src/pages/user/UserOrderDetailsPage.js
@@ -1,5 +1,28 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 function UserOrderDetailsPage() {
-  return <h1>This is the User order details page</h1>;
+  return (
+    <Container fluid>
+      <Row className="mt-4">
+        <h1>Order details</h1>
+        <Col md={8}>
+          <br />
+
+          <Row>
+            <Col md={6}>
+              <h2>Shipping</h2>
+            </Col>
+
+            <Col md={6}>
+              <h2>Payment method</h2>
+            </Col>
+          </Row>
+        </Col>
+
+        <h>Order Summary</h>
+      </Row>
+    </Container>
+  );
 }
 
 export default UserOrderDetailsPage;
