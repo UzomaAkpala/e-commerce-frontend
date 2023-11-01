64cfc494e9ba1d922aac91a6a4210744ae3f78c7
UzomaAkpala <francisakpala@yahoo.com>
2023-11-01 16:12:32 +0100

    Finish User Order Details Page

--- src/pages/user/UserOrderDetailsPage.js
+++ src/pages/user/UserOrderDetailsPage.js
@@ -1,4 +1,14 @@
-import { Container, Row, Col } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Alert,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from "react-bootstrap";
+import CartItemComponent from "../../components/CartItemComponent";
 
 function UserOrderDetailsPage() {
   return (
@@ -11,15 +21,73 @@ function UserOrderDetailsPage() {
           <Row>
             <Col md={6}>
               <h2>Shipping</h2>
+              <b>Name</b>: John Doe
+              <b>Address</b>: 8739 Mayflower St. Los Angeles, CA 90063
+              <b>Phone</b>: 888 777 66776
             </Col>
 
             <Col md={6}>
               <h2>Payment method</h2>
+              <Form.Select disabled={false}>
+                <option value="pp"> PayPal</option>
+                <option value="cod">
+                  {" "}
+                  Cash On Delivery (delivery may be delayed)
+                </option>
+              </Form.Select>
             </Col>
+            <Row>
+              <Col>
+                <Alert className="mt-3" variant="danger">
+                  Not delivered
+                </Alert>
+              </Col>
+              <Col>
+                <Alert className="mt-3" variant="success">
+                  Paid on 2023-10-31 Not delivered
+                </Alert>
+              </Col>
+            </Row>
           </Row>
+          <br />
+          <h2>Order items</h2>
+          <ListGroup variant="flush">
+            {Array.from({ length: 3 }).map((item, idx) => (
+              <CartItemComponent key={idx} />
+            ))}
+          </ListGroup>
         </Col>
+        <Col md={4}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h3>Order Summary</h3>
+            </ListGroup.Item>
 
-        <h>Order Summary</h>
+            <ListGroup.Item>
+              Items price (after tax): <span className="fw-bold">$892</span>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Shipping: <span className="fw-bold">included</span>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Tax: <span className="fw-bold">included</span>
+            </ListGroup.Item>
+
+            <ListGroup.Item className="text-danger">
+              Total Price: <span className="fw-bold">$904</span>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <div className="d-grid gap-2">
+                <Button size="lg" variant="danger" type="button">
+                  Pay for the order
+                </Button>
+              </div>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
       </Row>
     </Container>
   );
